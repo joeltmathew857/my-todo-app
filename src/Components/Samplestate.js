5f772d96b1a754f84846cc86beb6f5210f65458c
joel thomas mathew <joelmathew857@gmail.com>
2023-07-22 04:18:18 +0530

    aadded-occupation

--- src/Components/Samplestate.js
+++ src/Components/Samplestate.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-// PersonCard component receives 'name' and 'age' as props
-const PersonCard = ({ name, age }) => {
+// PersonCard component receives 'name', 'age', and 'occupation' as props
+const PersonCard = ({ name, age, occupation }) => {
   return (
     <div className="bg-gray-100 p-4 rounded-md shadow-md">
       <h2 className="text-xl font-semibold">{name}</h2>
       <p className="text-gray-600">Age: {age}</p>
+      <p className="text-gray-600">Occupation: {occupation}</p>
     </div>
   );
 };
--- src/Components/SearchItem.js
+++ src/Components/SearchItem.js
@@ -5,9 +5,9 @@ const App = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">People Information</h1>
-      <PersonCard name="John Doe" age="30" />
-      <PersonCard name="Jane Smith" age="25" />
-      <PersonCard name="Michael Johnson" age="40" />
+      <PersonCard name="John Doe" age="30" occupation="Software Engineer" />
+      <PersonCard name="Jane Smith" age="25" occupation="Data Scientist" />
+      <PersonCard name="Michael Johnson" age="40" occupation="Teacher" />
     </div>
   );
 };
