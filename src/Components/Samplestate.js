a0b15b4a9c227cf99f08250ab66f368e21449e19
joel thomas mathew <joelmathew857@gmail.com>
2023-07-22 04:10:59 +0530

    added-props

--- src/Components/Samplestate.js
+++ src/Components/Samplestate.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
-const Square = ({value, onSquareClick }) => {
-    return (
-        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 mr-2"
-        onClick={onSquareClick}     
-           >
-            {value}
-        </button>
-    );
+// PersonCard component receives 'name' and 'age' as props
+const PersonCard = ({ name, age }) => {
+  return (
+    <div className="bg-gray-100 p-4 rounded-md shadow-md">
+      <h2 className="text-xl font-semibold">{name}</h2>
+      <p className="text-gray-600">Age: {age}</p>
+    </div>
+  );
 };
 
-
+export default PersonCard;
